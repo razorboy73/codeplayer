9368a335acba45ba3fa923387cf79e14b9d258d6
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2021-07-16 06:51:58 -0400

    javascript for panels was installed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,33 @@
-document.getElementById("menuBarButton1").onclick = function(){
-    document.getElementById("codePanel1").style.display = "none";
-}
+
+
+$(".menuBarButton").hover(
+    function(){
+    $(this).addClass("highlightedButton")},
+    function(){
+    $(this).removeClass("highlightedButton")},
+)
+    
+$(".menuBarButton").click(
+    function(){
+        $(this).toggleClass("active");
+        $(this).removeClass("highlightedButton");
+        var panelId = $(this).attr("id")+"Panel";
+        alert(panelId);
+        $("#"+panelId).toggleClass("hidden");
+        var numberOfActivePanels = 4 - $(".hidden").length;
+        $(".panel").width(($(window).width()/numberOfActivePanels)-10);
+
+        
+        
+    })
+
+$(".panel").height($(window).height()-$("#topBar").height()-15);
+$(".panel").width(($(window).width()/2)-10);
+
+
+$("textarea").on('change keyup paste', function(){
+    $("iframe").contents().find("html").html($("#htmlPanel").val());
+});
+
+
+
